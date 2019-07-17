81c29c055a3401a25512b54a56fa50bafe36cc99
Sérgio Gonçalves <eu.sergiogoncalves@gmail.com>
2019-07-17 14:43:23 -0400

    Parei no modulo 6

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,8 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+        this.serverElements.push({
+          type: 'server',
+          name: serverData.serverName,
+          content: serverData.serverContent
+        });
+      }
 
+      onBluePrintedAdded(blueprintData: {serverName: string, serverContent: string}) {
+        this.serverElements.push({
+          type: 'blueprint',
+          name: blueprintData.serverName,
+          content: blueprintData.serverName
+        });
+      }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      console.log('Even');
+      this.evenNumbers.push(firedNumber);
+    } else {
+      console.log('Odd');
+      this.oddNumbers.push(firedNumber);
+    }
+  }
 
 }
--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,16 +1,74 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+                          implements OnInit,
+                                     OnChanges,
+                                     DoCheck,
+                                     AfterContentInit,
+                                     AfterContentChecked,
+                                     AfterViewInit,
+                                     AfterViewChecked,
+                                     OnDestroy {
+
  @Input('srvElement') element:{type: string, name: string, content: string};
 
-  constructor() { }
+ @Input() name: string;
+
+ @ViewChild('heading', {static: false}) header: ElementRef;
+ @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
+
+  constructor() {
+
+    console.log('Constructor Called');
+
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges Called');
+    console.log(changes);
+  }
+
 
   ngOnInit() {
+
+    console.log('ngOnInit Called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+    console.log('Paragraph content: ' + this.paragraph.nativeElement.textContent);
+
+  }
+
+  ngDoCheck(){
+
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit(){
+    console.log('Paragraph content: ' + this.paragraph.nativeElement.textContent);
+    console.log('ngAfterContentInit called!');
+
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called!');
+
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+  }
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy called!');
   }
 
 }
