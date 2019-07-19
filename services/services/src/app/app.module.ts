3af0825131c02f8351b15b42dc8fed42b8781553
Sergio <eu.sergiogoncalves@gmail.com>
2019-07-18 22:27:29 -0400

    continuando

---
+++ services/services/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccountComponent,
+
+    NewAccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
